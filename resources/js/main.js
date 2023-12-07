d0aaa38102fbc9d4bd1d6651568bfd378f4a9934
R.S Riody <153186765+ridoyrs27@users.noreply.github.com>
2023-12-07 22:36:20 +0600

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,14 @@
+// MIXITUP
+var mixer = mixitup('.work-grid');
+
+//Menu
+function openMenu(){
+   document.getElementById('manu-container').style.width = "100%";
+}
+
+function closeMenu(){
+   document.getElementById('manu-container').style.width = "0";
+}
+
+// ANIMATION
+new WOW().init();
